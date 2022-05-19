ddf6bb03b4eb356908392b9844ff7d6af70aa4a3
Carlos H. Campello <carlos@qualinfo.com.br>
2022-05-19 16:02:46 -0300

    Sweepstake duration, withdraw and balance fix

--- contracts/EsolidarSweepstake.sol
+++ contracts/EsolidarSweepstake.sol
@@ -28,9 +28,16 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
     address indexed owner
   );
   event SweepstakeDestroyed(uint256 indexed sweepstakeId);
-  event SweepstakeDraw(uint256 indexed sweepstakeId, uint256 indexed tokenId, address indexed winner, uint256 amount);
+  event SweepstakeDraw(
+    uint256 indexed sweepstakeId,
+    uint256 indexed tokenId,
+    address indexed winner,
+    uint256 amount
+  );
 
   event Stake(uint256 indexed sweepstakeId, address indexed staker, uint256 amount);
+
+  event BalanceUpdated(address indexed account, address indexed erc20token, uint256 amount);
 
   event Withdraw(address indexed account, uint256 amount);
 
@@ -54,10 +61,10 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
     uint256 cumulative;
   }
 
-  mapping(address => uint256) public balances;
-
   // mapping(uint256 => uint256) public tokenIdToSweepstakeId;
   mapping(uint256 => Sweepstake) public sweepstakes;
+
+  mapping(address => mapping(address => uint256)) public balances;
 
   mapping(address => mapping(uint256 => uint256)) public erc20TokenPerDonor; // donor address[sweepstake id] = erc20 token amount
   mapping(uint256 => address[]) public addressesPerSweepstake;
@@ -82,7 +89,7 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
     sweepstake.tokenId = _tokenId;
     sweepstake.owner = _owner;
     sweepstake.erc20Token = _erc20Token;
-    sweepstake.duration = _duration;
+    sweepstake.duration = uint64(block.timestamp) + uint64(_duration);
     sweepstake.active = true;
     sweepstake.destroyed = false;
 
@@ -90,8 +97,8 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
   }
 
   function destroy(uint256 _sweepstakeId) external onlyOwner nonReentrant {
-    require(!sweepstakes[_sweepstakeId].active, "SSESolidar: Not active");
-    require(sweepstakes[_sweepstakeId].destroyed, "SSESolidar: Already destroyed");
+    require(sweepstakes[_sweepstakeId].active, "SSESolidar: Not active");
+    require(!sweepstakes[_sweepstakeId].destroyed, "SSESolidar: Already destroyed");
 
     sweepstakes[_sweepstakeId].duration = 0;
     sweepstakes[_sweepstakeId].active = false;
@@ -107,7 +114,11 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
       assert(sweepstakes[_sweepstakeId].totalStakedTokens > 0);
 
       sweepstakes[_sweepstakeId].totalStakedTokens -= refund;
-      balances[addressesPerSweepstake[_sweepstakeId][i]] += refund;
+      _addBalance(
+        addressesPerSweepstake[_sweepstakeId][i],
+        sweepstakes[_sweepstakeId].erc20Token,
+        refund
+      );
     }
 
     emit SweepstakeDestroyed(_sweepstakeId);
@@ -115,6 +126,7 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
 
   // Stake
   function stake(uint256 _sweepstakeId, uint256 _amount) public nonReentrant {
+    require(_amount > 0, "SSESolidar: Stake amount must be greater than 0");
     require(sweepstakes[_sweepstakeId].active, "SSESolidar: Not active");
     require(sweepstakes[_sweepstakeId].owner != msg.sender, "SSESolidar: Owner cannot stake");
     if (
@@ -142,11 +154,14 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
     nonReentrant
     returns (uint256 ticket, address winner)
   {
-    // TO-DO: check sweepstake is active
-    // TO-DO: check sweepstake is not destroyed
-    // TO-DO: check there are stakers
+    require(sweepstakes[_sweepstakeId].active, "SSESolidar: Not active");
+    require(!sweepstakes[_sweepstakeId].destroyed, "SSESolidar: Already destroyed");
+    require(sweepstakes[_sweepstakeId].totalStakedTokens > 0, "SSESolidar: No one has staked");
+    require(block.timestamp > sweepstakes[_sweepstakeId].duration, "SSESolidar: Not ready to draw");
+    // TO-DO: only sweepstake owner can draw
+    // TO-DO: check if minimum stake is reached
+    // TO-DO: check if minimum value stake is reached
     // TO-DO: check there are enough stakers to draw
-    // TO-DO: check time has not passed
 
     ticket = _getPseudoRandomNumber(_sweepstakeId);
 
@@ -161,7 +176,11 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
         uint256 totalStaked = sweepstakes[_sweepstakeId].totalStakedTokens;
         // sweepstakes[_sweepstakeId].totalStakedTokens = 0; // Preserve this data
 
-        balances[sweepstakes[_sweepstakeId].owner] += totalStaked;
+        _addBalance(
+          sweepstakes[_sweepstakeId].owner,
+          sweepstakes[_sweepstakeId].erc20Token,
+          totalStaked
+        );
 
         ERC721contract.transferFrom(
           sweepstakes[_sweepstakeId].owner,
@@ -178,6 +197,33 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
         return (ticket, winner);
       }
     }
+  }
+
+  function _addBalance(
+    address _account,
+    address _erc20token,
+    uint256 _amount
+  ) internal {
+    balances[_account][_erc20token] += _amount;
+    emit BalanceUpdated(_account, _erc20token, balances[_account][_erc20token]);
+  }
+
+  function _subBalance(
+    address _account,
+    address _erc20token,
+    uint256 _amount
+  ) internal {
+    balances[_account][_erc20token] -= _amount;
+    emit BalanceUpdated(_account, _erc20token, balances[_account][_erc20token]);
+  }
+
+  function withdraw(address erc20Token) external nonReentrant {
+    require(balances[msg.sender][erc20Token] > 0, "SSESolidar: No balance");
+
+    uint256 balance = balances[msg.sender][erc20Token];
+    balances[msg.sender][erc20Token] = 0;
+
+    SafeERC20.safeTransfer(IERC20(erc20Token), msg.sender, balance);
   }
 
   function _getPseudoRandomNumber(uint256 _sweepstakeId)
@@ -205,8 +251,8 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
 
   // Views
 
-  function balanceOf(address account) public view returns (uint256 balance) {
-    return balances[account];
+  function balanceOf(address _account, address _erc20Token) public view returns (uint256 balance) {
+    return balances[_account][_erc20Token];
   }
 
   function getSweepstakeNumberOfDonors(uint256 _sweepstakeId) public view returns (uint256 donors) {
@@ -232,7 +278,11 @@ contract EsolidarSweepstake is Ownable, ReentrancyGuard {
     }
   }
 
-  function getSweepstakeWithDonors(uint256 _sweepstakeId) external view returns (Sweepstake memory sweepstakesWithDonor) {
+  function getSweepstakeWithDonors(uint256 _sweepstakeId)
+    external
+    view
+    returns (Sweepstake memory sweepstakesWithDonor)
+  {
     sweepstakesWithDonor = sweepstakes[_sweepstakeId];
     sweepstakesWithDonor.donors = getSweepstakeDonorsInfo(_sweepstakeId);
   }
