1dee1c206a72cf84031a79a061398242144fadc6
Carlos H. Campello <carlos@qualinfo.com.br>
2022-05-31 09:15:39 -0300

    Configure baseURI (commented)

--- scripts/configure-contracts.js
+++ scripts/configure-contracts.js
@@ -7,8 +7,15 @@ const { deployments, getNamedAccounts } = hre;
   const ERC721 = await hre.ethers.getContractFactory("ERC721EsolidarSweepstake");
   const erc721 = await ERC721.attach(contracts.ERC721EsolidarSweepstake.receipt.contractAddress);
 
-  const tx = await erc721.setSweepstakeAddress(
+  // Set sweepstake address on ERC721 contract
+  const tx1 = await erc721.setSweepstakeAddress(
     contracts.EsolidarSweepstake.receipt.contractAddress
   );
-  console.log("Tx", (await tx.wait()).transactionHash);
+  console.log("setSweepstakeAddress Tx", (await tx1.wait()).transactionHash);
+
+  // Configure baseURI on ERC721 contract
+  // const baseURI = "https://esolidar.com/";
+  // const tx2 = await erc721.setBaseURI(baseURI);
+  // console.log("setBaseURI Tx", (await tx2.wait()).transactionHash);
+
 })();
